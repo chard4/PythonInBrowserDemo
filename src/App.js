9e7f4676305f8fe7a17fce475872bb992fa8a5e2
Richard <captaincardo@gmail.com>
2023-01-23 13:40:55 -0800

    demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { showOpenFilePicker } from 'native-file-system-adapter';
+import { React, useState } from 'react';
+import { PythonProvider, usePython } from 'react-py';
+
+/*
+Minimal example of running a local Python file in the browser
+using the native-file-system-adapter ponyfill of the File System Access API and react-py
+*/
 
 function App() {
+  const [fileHandle, setFileHandle] = useState()
+  const [fileContents, setFileContents] = useState('')
+
+  const { runPython, stdout, stderr, isLoading, isRunning } = usePython();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <PythonProvider>
+    <div id="app" className="App">
+      <h1>Python In Browser Demo</h1>
+      <button onClick={async () => {
+        let unwrappedFileHandle;
+        [unwrappedFileHandle] = await showOpenFilePicker();
+        setFileHandle(unwrappedFileHandle);
+        const file = await unwrappedFileHandle.getFile();
+        setFileContents(await file.text());
+      }}>Load File</button>
+      <p>
+      <textarea 
+        type="text"
+        readOnly={true}
+        value={fileContents}
+        rows={10}
+        cols={60}
+      />
+      </p>
+        <button onClick={() => {
+          runPython(fileContents);
+        }}>Run Script</button>
+      <p>Output</p>
+      <pre>
+        <code>{stdout}</code>
+      </pre>
+      <p>Error</p>
+      <pre>
+        <code>{stderr}</code>
+      </pre>
     </div>
+    </PythonProvider>
   );
 }
 
